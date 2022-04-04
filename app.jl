1736c935408808f2e04c17e04bcc8135ab71fd92
ninpnin <vainoyrjanainen@icloud.com>
2022-04-04 18:49:01 +0300

    refactor: add comments

--- app.jl
+++ app.jl
@@ -1,4 +1,3 @@
-
 # Linear transformation
 linear(W, b, x) = W * x + b
 
@@ -8,16 +7,18 @@ relu(x) = max(0, x)
 # Layer with activation
 layer(W, b, activation, x) = activation.(linear(W, b, x))
 
-
+# Dimensionalities of the layers
 L = [3, 5, 2]
 weights = []
 
+# Initialize the weights
 for (D_i, D_i1) in zip(L[1:length(L)-1], L[2:length(L)])
 	W_0 = rand(D_i1,D_i) - ones(D_i1,D_i) * 0.5
 	b_0 = rand(D_i1) - ones(D_i1) * 0.5
 	push!(weights,(W_0, b_0))
 end
 
+# Define neural network
 function y_hat(x)
 	for (W, b) in weights
 		x = layer(W, b, relu, x)
@@ -25,6 +26,6 @@ function y_hat(x)
 	x
 end
 
+# Test drive
 x_0 = rand(L[1])
 print(y_hat(x_0))
-#y_hat(W1, W2, b1, b2, x)
